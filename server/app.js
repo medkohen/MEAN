30c7b084d35e10d83136a728f13b0eba679278ad
medkohen <simo.kohen1@gmail.com>
2021-05-03 20:49:00 +0200

    sans connextion a la DB

--- server/app.js
+++ server/app.js
@@ -14,35 +14,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 
 // mongodb+srv://user_01:<password>@cluster0.f480k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-mongoose.connect(
-  "mongodb+srv://user_01:Kohen123@cluster0.f480k.mongodb.net/mydb?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connexion opened to MongoDB!");
-    }
-  }
-);
-
-// const MongoClient = require("mongodb").MongoClient;
-// const uri =
-//   "mongodb+srv://user_01:Kohen123@cluster0.f480k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+// mongoose.connect(
+//   "mongodb+srv://user_01:Kohen123@cluster0.f480k.mongodb.net/mydb?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Connexion opened to MongoDB!");
+//     }
+//   }
+// );
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
